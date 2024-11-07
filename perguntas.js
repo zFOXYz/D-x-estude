121f9b9120c5e5ff24c1bf71a5a6576de4068b01
Samuel <00001097132596SP@al.educacao.sp.gov.br>
2024-11-07 08:57:15 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript?',
+    'O javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gato em Inglês?',
+    'Oi em ingles é cat'
+)
